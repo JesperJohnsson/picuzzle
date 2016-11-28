b6d0862cb15b5d07be59ff1b25ca59ca7e4e01cb
Dennis Rojas <dennis@refoto.se>
2016-11-28 15:32:53 +0100

    minor changes

--- picuzzle/GameViewController.swift
+++ picuzzle/GameViewController.swift
@@ -43,6 +43,7 @@ class GameViewController: UIViewController {
         /* Function for future use? */
         print("callback")
         //self.gameoverScene.setResult(result: points)
+        performSegue(withIdentifier: "back", sender: nil)
     }
 
     override var shouldAutorotate: Bool {
