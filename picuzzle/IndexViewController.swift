9f85857d5318d7fa14f1f79291346a548ec77020
Jesper Johnsson <jesper.johnsson1995@hotmail.com>
2016-12-06 21:52:47 +0100

    Added settings view and a way for the user to change their name

--- picuzzle/IndexViewController.swift
+++ picuzzle/IndexViewController.swift
@@ -3,9 +3,29 @@ import CoreData
 
 class IndexViewController: UIViewController {
 
+    @IBOutlet weak var informationTxtLbl: UILabel!
+    @IBOutlet weak var insertNameTxtFld: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var highscoreBtn: UIButton!
+    @IBOutlet weak var settingsBtn: UIButton!
+    
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        if let userName = defaults.string(forKey: "userNameKey") {
+            setUserNameInformation(name: userName)
+            insertNameTxtFld.isHidden = true
+            playBtn.isHidden = false
+            highscoreBtn.isHidden = false
+            settingsBtn.isHidden = false
+        } else {
+            playBtn.isHidden = true
+            highscoreBtn.isHidden = true
+            settingsBtn.isHidden = true
+        }
+    
         let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
         backgroundImage.image = UIImage(named: "background.png")
         self.view.insertSubview(backgroundImage, at: 0)
@@ -17,7 +37,6 @@ class IndexViewController: UIViewController {
         } else {
             print("Internet connection FAILED")
         }
-        
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,9 +47,29 @@ class IndexViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         hideNavigation()
+        
+        if let userName = defaults.string(forKey: "userNameKey") {
+            setUserNameInformation(name: userName)
+        }
     }
     
     func hideNavigation() {
         self.navigationController?.navigationBar.isHidden = true
+    }
+    
+    @IBAction func didEndOnExit(_ sender: UITextField) {
+        defaults.set(sender.text!, forKey: "userNameKey")
+        setUserNameInformation(name: sender.text!)
+        
+        insertNameTxtFld.isHidden = true
+        playBtn.isHidden = false
+        highscoreBtn.isHidden = false
+        settingsBtn.isHidden = false
+        
+        print(sender.text!)
+    }
+    
+    func setUserNameInformation(name: String) {
+        informationTxtLbl.text = "WELCOME \(name.uppercased())"
     }
 }
